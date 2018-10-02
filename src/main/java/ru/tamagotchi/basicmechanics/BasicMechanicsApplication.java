6a47c1e7e8e593acb6e88d7a144dfeaae995b161
Sergey Makarov <makar1031@yandex.ru>
2018-10-02 21:56:25 +0300

    изменено название микросервиса

--- src/main/java/ru/tamagotchi/basicmechanics/BasicMechanicsApplication.java
+++ src/main/java/ru/tamagotchi/basicmechanics/BasicMechanicsApplication.java
@@ -1,12 +1,11 @@
-package ru.tamagotchi.core;
+package ru.tamagotchi.basicmechanics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreApplication {
-
+public class BasicMechanicsApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApplication.class, args);
+		SpringApplication.run(BasicMechanicsApplication.class, args);
 	}
 }
