a1da97dbe2516570ee3ff75349391b0372453ca6
Sergey Makarov <makar1031@yandex.ru>
2018-10-07 13:51:20 +0300

    добавлена модель расписания

---
+++ src/main/java/ru/tamagotchi/basicmechanics/domain/ScheduleItem.java
@@ -0,0 +1,40 @@
+package ru.tamagotchi.basicmechanics.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+
+/**
+ * Created by makar
+ * 07.10.2018 12:45
+ */
+@Entity
+@Table(name = "SCHEDULE_ITEMS")
+@Data
+public class ScheduleItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalTime time;
+
+    @Column(nullable = false, length = 50)
+    private String description;
+
+    @Column(name = "IS_BEGINNING_OF_DAY", nullable = false)
+    private boolean beginningOfDay;
+
+    @Column(name = "IS_END_OF_DAY", nullable = false)
+    private boolean endOfDay;
+
+    @Column(name = "IS_DECREASE_HUNGER", nullable = false)
+    private boolean decreaseHunger;
+
+    @Column(name = "IS_DECREASE_REST", nullable = false)
+    private boolean decreaseRest;
+
+    @Column(name = "IS_DECREASE_MOOD", nullable = false)
+    private boolean decreaseMood;
+}
---
+++ src/main/resources/db/migration/V2__create_schedule-items.sql
@@ -0,0 +1,29 @@
+CREATE TABLE SCHEDULE_ITEMS (
+    ID BIGSERIAL PRIMARY KEY,
+    TIME TIME NOT NULL,
+    DESCRIPTION varchar(50) NOT NULL,
+    IS_BEGINNING_OF_DAY BOOLEAN NOT NULL,
+    IS_END_OF_DAY BOOLEAN NOT NULL,
+    IS_DECREASE_HUNGER BOOLEAN NOT NULL,
+    IS_DECREASE_REST BOOLEAN NOT NULL,
+    IS_DECREASE_MOOD BOOLEAN NOT NULL
+);
+CREATE UNIQUE INDEX SCHEDULE_ITEMS_TIME_uindex ON SCHEDULE_ITEMS (TIME);
+
+INSERT INTO SCHEDULE_ITEMS (
+    TIME,
+    DESCRIPTION,
+    IS_BEGINNING_OF_DAY,
+    IS_END_OF_DAY,
+    IS_DECREASE_HUNGER,
+    IS_DECREASE_REST,
+    IS_DECREASE_MOOD
+) VALUES
+    ('06:00', 'начало дня, персонаж просыпается', TRUE, FALSE, FALSE, FALSE, FALSE),
+    ('09:00', 'первый приём пищи (завтрак)', FALSE, FALSE, TRUE, FALSE, FALSE),
+    ('10:00', 'первое игровое занятие', FALSE, FALSE, FALSE, FALSE, TRUE),
+    ('13:00', 'второй приём пищи (обед)', FALSE, FALSE, TRUE, FALSE, FALSE),
+    ('17:00', 'третий приём пищи (полдник)', FALSE, FALSE, TRUE, FALSE, FALSE),
+    ('18:00', 'второе игровое занятие', FALSE, FALSE, FALSE, FALSE, TRUE),
+    ('20:00', 'четвёртый приём пищи (ужин)', FALSE, FALSE, TRUE, FALSE, FALSE),
+    ('21:00', 'конец дня, персонаж отправляется на отдых', FALSE, TRUE, FALSE, TRUE, FALSE);
