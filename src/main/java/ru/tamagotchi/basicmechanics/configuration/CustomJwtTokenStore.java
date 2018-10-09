38054f9a09cde551f5cdc34b45962f99050fb600
Sergey Makarov <makar1031@yandex.ru>
2018-10-09 20:08:00 +0300

    проверка токена на наличие поля user_id

--- src/main/java/ru/tamagotchi/basicmechanics/configuration/CustomJwtTokenStore.java
+++ src/main/java/ru/tamagotchi/basicmechanics/configuration/CustomJwtTokenStore.java
@@ -1,9 +1,12 @@
 package ru.tamagotchi.basicmechanics.configuration;
 
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Map;
 
 /**
  * Created by makar
@@ -17,7 +20,16 @@ public class CustomJwtTokenStore extends JwtTokenStore {
     @Override
     public OAuth2Authentication readAuthentication(OAuth2AccessToken token) {
         OAuth2Authentication oAuth2Authentication = super.readAuthentication(token);
-        oAuth2Authentication.setDetails(token.getAdditionalInformation());
+        Map<String, Object> information = token.getAdditionalInformation();
+        checkUserIdClaim(information);
+        oAuth2Authentication.setDetails(information);
         return oAuth2Authentication;
+    }
+
+    private void checkUserIdClaim(Map<String, Object> information) {
+        Object userId = information.get("user_id");
+        if (!(userId instanceof Integer)) {
+            throw new InvalidTokenException("invalid token");
+        }
     }
 }
