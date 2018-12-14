defc323a305b73bcd4b354b97b42e1a239f16182
Sergey Makarov <makar1031@yandex.ru>
2018-12-14 21:25:53 +0300

    добавлена возможность выполнения авторизованных запросов в swagger

--- src/main/java/ru/tamagotchi/basicmechanics/configuration/SwaggerConfiguration.java
+++ src/main/java/ru/tamagotchi/basicmechanics/configuration/SwaggerConfiguration.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by makar
@@ -21,6 +23,10 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class SwaggerConfiguration {
 
+    private static final String TOKEN = "token";
+    private static final String HEADER_NAME = "Authorization";
+    private static final String PASS_AS = "header";
+
     private final VersionProperties versionProperties;
 
     @Bean
@@ -30,7 +36,21 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("ru.tamagotchi"))
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
                 .apiInfo(apiInfo());
+    }
+
+    private List<SecurityScheme> securitySchemes() {
+        return Collections.singletonList(new ApiKey(TOKEN, HEADER_NAME, PASS_AS));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        SecurityContext context = SecurityContext.builder()
+                .securityReferences(Collections.singletonList(new SecurityReference(TOKEN, new AuthorizationScope[0])))
+                .forPaths(PathSelectors.regex("/pet.*"))
+                .build();
+        return Collections.singletonList(context);
     }
 
     private ApiInfo apiInfo() {
